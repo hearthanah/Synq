087d22a5b0ca4536646863bcf6247f72d7dc2ce3
mbcarpenter <mattcarpenter929@gmail.com>
2015-11-09 17:11:38 -0600

    Copied and pasted old UI that got commited over, and old class files for the views and view controllers in that UI

---
+++ Synq/ActiveSongVC.swift
@@ -0,0 +1,40 @@
+//
+//  ActiveSongVC.swift
+//  Synq
+//
+//  Created by Matthew Carpenter on 10/18/15.
+//  Copyright © 2015 Group11. All rights reserved.
+//
+
+import UIKit
+
+class ActiveSongVC: UIViewController {
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var trackLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
